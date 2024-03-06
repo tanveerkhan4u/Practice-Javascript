d4dcbaa3c9312741ea1acf0c9ee9144aceee1c74
Mohd Tanveer Khan <khantanveer6077@gmail.com>
2024-03-06 20:33:38 +0530

    update

--- script.js
+++ script.js
@@ -4,5 +4,8 @@
 // let username = 'iron man';
 // console.log(username);
 
-let userIntro = "my name is khan";
-console.log(userIntro);
+// let userIntro = "my name is khan";
+// console.log(userIntro);
+
+let age = 25;
+console.log(age);
