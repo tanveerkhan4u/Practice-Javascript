4d87d41a43d6ab1c39359176af55849083a775a5
Mohd Tanveer Khan <khantanveer6077@gmail.com>
2024-03-06 20:27:46 +0530

    update

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // console.log("word");
 // string data type  
 
-let username = 'iron man';
-console.log(username);
+// let username = 'iron man';
+// console.log(username);
 
-let y = "my name is khan";
-console.log(y);
+let userIntro = "my name is khan";
+console.log(userIntro);
